93ed3c4a89dd2b5dfd146e8192216c96baca8a0f
yd2058 <yd2058@bs.amalnet.k12.il>
2025-06-12 00:52:57 +0300

    requires map fix - and javadoc

--- app/src/main/java/com/example/pancentbeta1/HomeActivity.java
+++ app/src/main/java/com/example/pancentbeta1/HomeActivity.java
@@ -8,13 +8,24 @@ package com.example.pancentbeta1;
  */
 
 
+import static android.graphics.Color.BLUE;
+import static android.graphics.Color.TRANSPARENT;
 import static com.example.pancentbeta1.Helpers.FBHelper.refauth;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -25,13 +36,62 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.pancentbeta1.Helpers.Calibration;
 
+/**
+ * The type Home activity.
+ */
 public class HomeActivity extends AppCompatActivity {
+
+    TextView hmTv;
+    DownloadManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        hmTv = findViewById(R.id.hmTv);
+
+        String part1 = "Welcome " + (refauth.getCurrentUser() != null ? refauth.getCurrentUser().getDisplayName() : "Guest") + "!\n";
+        String part2 = "this is my experiment, it is woth noting that the speaker location based calibration is innacurate due to GPS inaccuracy.\n ";
+        String clickableText = "click here";
+        String part3 = " for the file for sound based calibration!";
+
+        String fullText = part1 + part2 + clickableText + part3;
+
+        SpannableString spannableString = new SpannableString(fullText);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                Uri uri = Uri.parse("https://drive.google.com/uc?export=download&id=1GHqb9AubBGPEr55sqCUkl5uYi9j1M6-z");
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+                long reference = manager.enqueue(request);
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true); // Make it look like a link
+                ds.setColor(BLUE);     // Set link color to blue
+            }
+        };
+
+        int startIndex = fullText.indexOf(clickableText);
+        int endIndex = startIndex + clickableText.length();
+
+        if (startIndex != -1) { // Ensure "click here" was found
+            spannableString.setSpan(clickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+
+        hmTv.setText(spannableString);
+        hmTv.setMovementMethod(LinkMovementMethod.getInstance()); // !IMPORTANT to make links clickable
+        hmTv.setHighlightColor(TRANSPARENT); // Optional: removes the highlight color on click
+
     }
+
+
 
     public boolean onCreateOptionsMenu(Menu menu){
         menu.add("Home");
@@ -65,6 +125,11 @@ public class HomeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * S pl.
+     *
+     * @param view the view
+     */
     public void sPl(View view) {
         Intent intent = new Intent(this, CalibrationActivity.class);
         intent.putExtra("isLocation",true);
@@ -72,6 +137,11 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * S pan.
+     *
+     * @param view the view
+     */
     public void sPan(View view) {
         Intent intent = new Intent(this, CalibrationActivity.class);
         intent.putExtra("isLocation",false);
